50d790e2157a688d0c50002e16861212336fcfa3
Quidam74 <toutlemonde74@gmail.com>
2019-07-05 23:30:51 +0200

    probleme request observable

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,13 +11,33 @@
     components: {},
     props: {},
     mounted () {
-      this.getAllChips()
-      this.getAllWeapon()
+      this.getAllVar()
     },
     data () {
       return {
         chips: null,
-        weapon: null
+        weapon: null,
+        constant: null,
+        chipsDamage: null,
+        chipsHeal: null,
+        chipsBoost: null,
+        chipsShield: null,
+        chipsTactic: null,
+        chipsReflect: null,
+        chipsPoison: null,
+        chipsPuny: null,
+        chipsDebuf: null,
+        typeDamage: null,
+        typeHeal: null,
+        typeBoost: null,
+        typeAbsolutShield: null,
+        typeRelativeShield: null,
+        typeTactic: null,
+        typeReflect: null,
+        typePoison: null,
+        typePuny: null,
+        typeDebuf: null
+
       }
     },
     methods: {
@@ -26,7 +46,12 @@
           .then(function (response) {
             response.json()
               .then(function (data) {
-                this.chips = JSON.stringify(data.chips)
+                this.chips = data.chips
+
+                console.log(this.chips)
+                this.chips.forEach((elem, index) => {
+                  console.log(elem)
+                })
               }.bind(this))
           }.bind(this))
       },
@@ -36,6 +61,53 @@
             response.json()
               .then(function (data) {
                 this.weapon = data.weapons
+              }.bind(this))
+          }.bind(this))
+      },
+      getAllVar () {
+        fetch('https://leekwars.com/api/constant/get-all')
+          .then(function (response) {
+            response.json()
+              .then(function (data) {
+                this.constant = data.constants
+                this.constant.forEach((elem, index) => {
+                  switch (elem.name) {
+                    case 'EFFECT_DAMAGE':
+                      this.typeDamage = elem
+                      break
+                    case 'EFFECT_HEAL':
+                      this.typeHeal = elem
+                      break
+                    case 'BUFF':
+                      // plus compliqué, todo
+                      this.typeBoost = elem
+                      break
+                    case 'EFFECT_ABSOLUTE_SHIELD':
+                      this.typeAbsolutShield = elem
+                      break
+                    case 'EFFECT_RELATIVE_SHIELD':
+                      this.typeRelativeShield = elem
+                      break
+                    case 'Tactic':
+                      // plus compliqué, todo pour le relative
+                      this.typeTactic = elem
+                      break
+                    case 'EFFECT_DAMAGE_RETURN':
+                      this.typeReflect = elem
+                      break
+                    case 'EFFECT_POISON':
+                      this.typePoison = elem
+                      break
+                    case 'EFFECT_SUMMON':
+                      this.typePuny = elem
+                      break
+                    case 'EFFECT_DEBUFF':
+                      this.typeDebuf = elem
+                      break
+                  }
+                })
+                this.getAllWeapon()
+                this.getAllChips()
               }.bind(this))
           }.bind(this))
       }
