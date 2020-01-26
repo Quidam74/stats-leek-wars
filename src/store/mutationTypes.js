75620dee8c7656859c88e1d3e753fd3f6b3f6db8
Quidam74 <toutlemonde74@gmail.com>
2020-01-26 18:01:39 +0100

    clean useless files

--- src/store/mutationTypes.js
+++ src/store/mutationTypes.js
@@ -1,12 +1,3 @@
-// notes.js
-export const SET_NOTES_LIST = 'notes/SET_NOTES_LIST'
-export const ADD_NOTE_TO_LIST = 'notes/ADD_NOTE_TO_LIST'
-export const UPDATE_NOTE_IN_LIST = 'notes/UPDATE_NOTE_IN_LIST'
-export const DELETE_NOTE_IN_LIST = 'notes/DELETE_NOTE_IN_LIST'
-
-export const SET_NOTE_ID_EDIT = 'notes/SET_NOTE_EDIT'
-export const SET_DISPLAY_EDIT = 'notes/SET_DISPLAY_EDIT'
-
 // leekWeapon.js
 export const ADD_LEEKWEAPON_TO_LIST = 'leekWeapon/ADD_LEEKWEAPON_TO_LIST'
 export const REMOVE_LEEKWEAPON_TO_LIST = 'leekWeapon/REMOVE_LEEKWEAPON_TO_LIST'
