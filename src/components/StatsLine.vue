d725cbc659a57f1083917b0c4414dda363ee79eb
Quidam74 <toutlemonde74@gmail.com>
2020-02-23 18:07:37 +0100

    move up calc capital

--- src/components/StatsLine.vue
+++ src/components/StatsLine.vue
@@ -4,9 +4,9 @@
         <h3 class="stat-name">{{stat}}</h3>
         <input v-on:change="setStoreValue" class="stat-input" type="number" v-model="value">
         <div class="stat-rapideChange">
-            <div v-on:click="addFixValue(1)">+1</div>
-            <div v-on:click="addFixValue(10)">+10</div>
-            <div v-on:click="addFixValue(100)">+100</div>
+            <div class="stat-rapideChangeButton" v-on:click="addFixValue(1)">+1</div>
+            <div class="stat-rapideChangeButton" v-on:click="addFixValue(10)">+10</div>
+            <div class="stat-rapideChangeButton" v-on:click="addFixValue(100)">+100</div>
         </div>
     </div>
     <div class="stat" v-else-if="stat ==='level'">
@@ -18,7 +18,7 @@
     <div class="stat" v-else-if="stat ==='capital'">
         <div class="stat-img"></div>
         <h3 class="stat-name">{{stat}}</h3>
-        <input disabled="disabled" class="stat-input" type="number" v-model="value">
+        <input disabled="disabled" class="stat-input" type="number" v-model="capital">
         <div class="stat-rapideChange"></div>
     </div>
 </template>
@@ -30,16 +30,23 @@
       typeStats: {
         type: [String],
         default: 'default'
+      },
+      totalCapital: {
+        type: [Number],
+        default: -1
       }
     },
     data () {
       return {
         stat: this.typeStats,
+        capital: this.totalCapital,
         value: null
       }
     },
     mounted () {
-      this.getValue(this.stat)
+      if (this.stat !== 'capital') {
+        this.getValue(this.stat)
+      }
     },
     methods: {
       addFixValue (number) {
@@ -80,8 +87,8 @@
           case 'agility':
             this.value = this.$store.getters.getAgility
             break
-          case 'capital':
-            this.value = this.getCapital()
+          default:
+            console.log('error')
             break
         }
       },
@@ -131,75 +138,6 @@
             break
         }
         this.$emit('capitalCalc')
-      },
-      getCapital () {
-        let level = this.$store.getters.getLevel
-        let totalCapital = 5 * level + 45 * (1 + parseInt(level / 100)) + (level === 301 ? 95 : 0)
-        let freq = this.$store.getters.getFrequency
-        let life = this.$store.getters.getLife
-        let magic = this.$store.getters.getMagic
-        let mp = this.$store.getters.getMp
-        let res = this.$store.getters.getResistance
-        let science = this.$store.getters.getScience
-        let str = this.$store.getters.getStrength
-        let tp = this.$store.getters.getTp
-        let wisdom = this.$store.getters.getwisdom
-        let agi = this.$store.getters.getAgility
-        let capital = totalCapital - this.calcCostLife(life) - this.calcCostBasicStats(str) - this.calcCostBasicStats(wisdom) - this.calcCostBasicStats(agi) - this.calcCostBasicStats(res) - this.calcCostBasicStats(science) - this.calcCostBasicStats(magic) - (freq - 100) - this.calcCostMp(mp) - this.calcCostTp(tp)
-        return capital
-      },
-      calcCostBasicStats (stat) {
-        // stat<200 1 : 2 200<=stat<400 =1 : 1 400<=stat<600 2:1 600>=stat 3:1
-        // force sagesse resi agi science magie
-        if (stat === null) {
-          return 0
-        }
-        let palier = 200
-        let totalPoint = 0
-        if (stat < palier) {
-          totalPoint += parseInt(stat) * 0.5
-        }
-        if (stat >= palier && stat < palier * 2) {
-          totalPoint += 100 + parseInt(stat) - palier
-        }
-        if (stat >= (palier * 2) && stat < palier * 3) {
-          totalPoint += 100 + palier + (parseInt(stat) - palier * 2) / 2 + 3
-        }
-        if (stat >= 600) {
-          totalPoint += 100 + palier + palier * 2 + (parseInt(stat) - palier * 3) / 2
-        }
-        return totalPoint
-      },
-      calcCostLife (effectiveLife) {
-        let effectifUseInPoint = effectiveLife - ((this.$store.getters.getLevel - 1) * 3 + 100)
-        let palier = 1000
-        let totalPoint = 0
-        if (effectifUseInPoint < 1000) {
-          totalPoint += effectifUseInPoint / 4
-        } else if (effectifUseInPoint >= 1000 && effectifUseInPoint < 2000) {
-          totalPoint = palier / 4 + (effectifUseInPoint - palier) / 3
-        } else if (effectifUseInPoint >= 2000) {
-          totalPoint = palier / 4 + palier / 3 + (effectifUseInPoint - palier * 2) / 2
-        }
-        return totalPoint
-      },
-      calcCostTp (stat) {
-        let totalPoint = 0
-        let baseCost = 30
-        let scale = 5
-        for (let i = 0; i < stat - 10; i++) {
-          totalPoint += baseCost + i * scale
-        }
-        return totalPoint
-      },
-      calcCostMp (stat) {
-        let totalPoint = 0
-        let baseCost = 20
-        let scale = 10
-        for (let i = 0; i < stat - 3; i++) {
-          totalPoint += baseCost + i * scale
-        }
-        return totalPoint
       }
     }
   }
@@ -221,13 +159,17 @@
             display: flex;
             width: 131px;
 
-            div {
+            &Button {
                 margin: 0 5px;
                 padding: 5px;
                 cursor: pointer;
                 background-color: #5fad1b;
                 color: #ffffff;
                 border-radius: 5px;
+
+                &-disable {
+
+                }
             }
         }
 
