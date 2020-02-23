4fc2360f8329c7487588fb405295d197e95573e7
Quidam74 <toutlemonde74@gmail.com>
2020-02-23 18:32:09 +0100

    disable button on not enougth capital ok

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -4,7 +4,7 @@
             <h2>Caractéristiques</h2>
             <span class="stats-resetButton" v-on:click="resetStats">Reset</span></div>
         <stats-line :key="watcher+item" v-on:capitalCalc="capitalCalc" v-for="item in stats"
-                    :typeStats="item" :total-capital="getCapital()"></stats-line>
+                    :typeStats="item"></stats-line>
     </div>
 </template>
 
@@ -36,75 +36,6 @@
     methods: {
       capitalCalc () {
         this.watcher = !this.watcher
-      },
-      getCapital () {
-        let level = this.$store.getters.getLevel
-        let totalCapital = 5 * level + 45 * (1 + parseInt(level / 100)) + (level === 301 ? 95 : 0)
-        let freq = this.$store.getters.getFrequency
-        let life = this.$store.getters.getLife
-        let magic = this.$store.getters.getMagic
-        let mp = this.$store.getters.getMp
-        let res = this.$store.getters.getResistance
-        let science = this.$store.getters.getScience
-        let str = this.$store.getters.getStrength
-        let tp = this.$store.getters.getTp
-        let wisdom = this.$store.getters.getwisdom
-        let agi = this.$store.getters.getAgility
-        let capital = totalCapital - this.calcCostLife(life) - this.calcCostBasicStats(str) - this.calcCostBasicStats(wisdom) - this.calcCostBasicStats(agi) - this.calcCostBasicStats(res) - this.calcCostBasicStats(science) - this.calcCostBasicStats(magic) - (freq - 100) - this.calcCostMp(mp) - this.calcCostTp(tp)
-        return capital
-      },
-      calcCostBasicStats (stat) {
-        // stat<200 1 : 2 200<=stat<400 =1 : 1 400<=stat<600 2:1 600>=stat 3:1
-        // force sagesse resi agi science magie
-        if (stat === null) {
-          return 0
-        }
-        let palier = 200
-        let totalPoint = 0
-        if (stat < palier) {
-          totalPoint += parseInt(stat) * 0.5
-        }
-        if (stat >= palier && stat < palier * 2) {
-          totalPoint += 100 + parseInt(stat) - palier
-        }
-        if (stat >= (palier * 2) && stat < palier * 3) {
-          totalPoint += 100 + palier + (parseInt(stat) - palier * 2) / 2 + 3
-        }
-        if (stat >= 600) {
-          totalPoint += 100 + palier + palier * 2 + (parseInt(stat) - palier * 3) / 2
-        }
-        return totalPoint
-      },
-      calcCostLife (effectiveLife) {
-        let effectifUseInPoint = effectiveLife - ((this.$store.getters.getLevel - 1) * 3 + 100)
-        let palier = 1000
-        let totalPoint = 0
-        if (effectifUseInPoint < 1000) {
-          totalPoint += effectifUseInPoint / 4
-        } else if (effectifUseInPoint >= 1000 && effectifUseInPoint < 2000) {
-          totalPoint = palier / 4 + (effectifUseInPoint - palier) / 3
-        } else if (effectifUseInPoint >= 2000) {
-          totalPoint = palier / 4 + palier / 3 + (effectifUseInPoint - palier * 2) / 2
-        }
-        return totalPoint
-      },
-      calcCostTp (stat) {
-        let totalPoint = 0
-        let baseCost = 30
-        let scale = 5
-        for (let i = 0; i < stat - 10; i++) {
-          totalPoint += baseCost + i * scale
-        }
-        return totalPoint
-      },
-      calcCostMp (stat) {
-        let totalPoint = 0
-        let baseCost = 20
-        let scale = 10
-        for (let i = 0; i < stat - 3; i++) {
-          totalPoint += baseCost + i * scale
-        }
-        return totalPoint
       },
       resetStats () {
         this.$store.dispatch('setLevel', 1)
--- src/components/StatsLine.vue
+++ src/components/StatsLine.vue
@@ -4,21 +4,28 @@
         <h3 class="stat-name">{{stat}}</h3>
         <input v-on:change="setStoreValue" class="stat-input" type="number" v-model="value">
         <div class="stat-rapideChange">
-            <div class="stat-rapideChangeButton" v-on:click="addFixValue(1)">+1</div>
-            <div class="stat-rapideChangeButton" v-on:click="addFixValue(10)">+10</div>
-            <div class="stat-rapideChangeButton" v-on:click="addFixValue(100)">+100</div>
+            <div class="stat-rapideChangeButton" v-bind:class="{ 'stat-rapideChangeButton-disable': isAbleButton(1) }"
+                 v-on:click="addFixValue(1,isAbleButton(1))">+1
+            </div>
+            <div class="stat-rapideChangeButton" v-bind:class="{ 'stat-rapideChangeButton-disable': isAbleButton(10) }"
+                 v-on:click="addFixValue(10,isAbleButton(10))">+10
+            </div>
+            <div class="stat-rapideChangeButton" v-bind:class="{ 'stat-rapideChangeButton-disable': isAbleButton(100) }"
+                 v-on:click="addFixValue(100,isAbleButton(100))">
+                +100
+            </div>
         </div>
+    </div>
+    <div class="stat" v-else-if="stat ==='capital'">
+        <div class="stat-img"></div>
+        <h3 class="stat-name">{{stat}}</h3>
+        <input disabled="disabled" class="stat-input" type="number" v-model="value">
+        <div class="stat-rapideChange"></div>
     </div>
     <div class="stat" v-else-if="stat ==='level'">
         <div class="stat-img"></div>
         <h3 class="stat-name">{{stat}}</h3>
         <input v-on:change="setStoreValue" class="stat-input" type="number" v-model="value">
-        <div class="stat-rapideChange"></div>
-    </div>
-    <div class="stat" v-else-if="stat ==='capital'">
-        <div class="stat-img"></div>
-        <h3 class="stat-name">{{stat}}</h3>
-        <input disabled="disabled" class="stat-input" type="number" v-model="capital">
         <div class="stat-rapideChange"></div>
     </div>
 </template>
@@ -30,27 +37,22 @@
       typeStats: {
         type: [String],
         default: 'default'
-      },
-      totalCapital: {
-        type: [Number],
-        default: -1
       }
     },
     data () {
       return {
         stat: this.typeStats,
-        capital: this.totalCapital,
         value: null
       }
     },
     mounted () {
-      if (this.stat !== 'capital') {
-        this.getValue(this.stat)
-      }
+      this.getValue(this.stat)
     },
     methods: {
-      addFixValue (number) {
-        this.setStoreValue(number)
+      addFixValue (number, buttonIsDisable) {
+        if (!buttonIsDisable) {
+          this.setStoreValue(number)
+        }
       },
       getValue (stat) {
         switch (stat) {
@@ -86,6 +88,9 @@
             break
           case 'agility':
             this.value = this.$store.getters.getAgility
+            break
+          case 'capital':
+            this.value = this.getCapital()
             break
           default:
             console.log('error')
@@ -138,6 +143,93 @@
             break
         }
         this.$emit('capitalCalc')
+      },
+      getCapital () {
+        let level = this.$store.getters.getLevel
+        let totalCapital = 5 * level + 45 * (1 + parseInt(level / 100)) + (level === 301 ? 95 : 0)
+        let freq = this.$store.getters.getFrequency
+        let life = this.$store.getters.getLife
+        let magic = this.$store.getters.getMagic
+        let mp = this.$store.getters.getMp
+        let res = this.$store.getters.getResistance
+        let science = this.$store.getters.getScience
+        let str = this.$store.getters.getStrength
+        let tp = this.$store.getters.getTp
+        let wisdom = this.$store.getters.getwisdom
+        let agi = this.$store.getters.getAgility
+        let capital = totalCapital - this.calcCostLife(life) - this.calcCostBasicStats(str) - this.calcCostBasicStats(wisdom) - this.calcCostBasicStats(agi) - this.calcCostBasicStats(res) - this.calcCostBasicStats(science) - this.calcCostBasicStats(magic) - (freq - 100) - this.calcCostMp(mp) - this.calcCostTp(tp)
+        return capital
+      },
+      calcCostBasicStats (stat) {
+        // stat<200 1 : 2 200<=stat<400 =1 : 1 400<=stat<600 2:1 600>=stat 3:1
+        // force sagesse resi agi science magie
+        if (stat === null) {
+          return 0
+        }
+        let palier = 200
+        let totalPoint = 0
+        if (stat < palier) {
+          totalPoint += parseInt(stat) * 0.5
+        }
+        if (stat >= palier && stat < palier * 2) {
+          totalPoint += 100 + parseInt(stat) - palier
+        }
+        if (stat >= (palier * 2) && stat < palier * 3) {
+          totalPoint += 100 + palier + (parseInt(stat) - palier * 2) / 2 + 3
+        }
+        if (stat >= 600) {
+          totalPoint += 100 + palier + palier * 2 + (parseInt(stat) - palier * 3) / 2
+        }
+        return totalPoint
+      },
+      calcCostLife (effectiveLife) {
+        let effectifUseInPoint = effectiveLife - ((this.$store.getters.getLevel - 1) * 3 + 100)
+        let palier = 1000
+        let totalPoint = 0
+        if (effectifUseInPoint < 1000) {
+          totalPoint += effectifUseInPoint / 4
+        } else if (effectifUseInPoint >= 1000 && effectifUseInPoint < 2000) {
+          totalPoint = palier / 4 + (effectifUseInPoint - palier) / 3
+        } else if (effectifUseInPoint >= 2000) {
+          totalPoint = palier / 4 + palier / 3 + (effectifUseInPoint - palier * 2) / 2
+        }
+        return totalPoint
+      },
+      calcCostTp (stat) {
+        let totalPoint = 0
+        let baseCost = 30
+        let scale = 5
+        for (let i = 0; i < stat - 10; i++) {
+          totalPoint += baseCost + i * scale
+        }
+        return totalPoint
+      },
+      calcCostMp (stat) {
+        let totalPoint = 0
+        let baseCost = 20
+        let scale = 10
+        for (let i = 0; i < stat - 3; i++) {
+          totalPoint += baseCost + i * scale
+        }
+        return totalPoint
+      },
+      isAbleButton (addedValue) {
+        let cost = 0
+        switch (this.stat) {
+          case 'mp':
+            cost = this.calcCostMp(this.value + addedValue) - this.calcCostMp(this.value)
+            break
+          case 'tp':
+            cost = this.calcCostTp(this.value + addedValue) - this.calcCostTp(this.value)
+            break
+          case 'life':
+            cost = this.calcCostLife(this.value + addedValue) - this.calcCostLife(this.value)
+            break
+          default:
+            cost = this.calcCostBasicStats(this.value + addedValue) - this.calcCostBasicStats(this.value)
+            break
+        }
+        return this.getCapital() <= cost
       }
     }
   }
@@ -168,7 +260,7 @@
                 border-radius: 5px;
 
                 &-disable {
-
+                    opacity: 0.3;
                 }
             }
         }
